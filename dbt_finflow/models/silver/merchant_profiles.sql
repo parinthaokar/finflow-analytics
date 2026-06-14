fe1972c01ae771dfdbca03c7d02ec4c749f26b80
Parin <parinthaokar@gmail.com>
2026-06-14 14:42:02 -0400

    feat: add silver merchant_summary model with fraud rate calculation

---
+++ dbt_finflow/models/silver/merchant_profiles.sql
@@ -0,0 +1,12 @@
+with step_one as (
+    SELECT * from {{ ref('stg_transactions') }}
+),
+final as (
+   select merchant_id, COUNT(transaction_id) as Total_Transactions, avg(amount) as Average_Transaction_Amount, max(amount) as max_transaction_amount,
+    SUM(case when is_fraud then 1 else 0 END ) as total_fraud_flags, count(distinct user_id) as distinct_users, (SUM(case when is_fraud then 1 else 0 end)::float / COUNT(transaction_id)) * 100 as fraud_rate,
+    CURRENT_TIMESTAMP() as processed_at
+    from step_one
+    group by merchant_id
+)
+
+select * from final
---
+++ dbt_finflow/target/run/dbt_finflow/models/silver/merchant_profiles.sql
@@ -0,0 +1,25 @@
+
+  
+    
+
+create or replace transient table FINFLOW.SILVER_SILVER.merchant_profiles
+    
+    
+    
+    as (with step_one as (
+    SELECT * from FINFLOW.SILVER_SILVER.stg_transactions
+),
+final as (
+   select merchant_id, COUNT(transaction_id) as Total_Transactions, avg(amount) as Average_Transaction_Amount, max(amount) as max_transaction_amount,
+    SUM(case when is_fraud then 1 else 0 END ) as total_fraud_flags, count(distinct user_id) as distinct_users, (SUM(case when is_fraud then 1 else 0 end)::float / COUNT(transaction_id)) * 100 as fraud_rate,
+    CURRENT_TIMESTAMP() as processed_at
+    from step_one
+    group by merchant_id
+)
+
+select * from final
+    )
+;
+
+
+  
